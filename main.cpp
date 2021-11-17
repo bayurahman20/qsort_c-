d550743d10cad5c9ebd9f4550a72f23d5d583717
devbayu20 <v130421015@bussan.co.id>
2021-11-18 00:36:12 +0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,68 +1,58 @@
 #include <iostream>
+#include <iomanip>
+#include <time.h>
 
 using namespace std;
- 
-// A utility function to swap two elements
-void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
- 
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
-int partition (int arr[], int low, int high) {
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
- 
-    for (int j = low; j <= high- 1; j++)
-    {
-        // If current element is smaller than or
-        // equal to pivot
-        if (arr[j] <= pivot)
-        {
-            i++;    // increment index of smaller element
-            swap(&arr[i], &arr[j]);
+
+int compareArray(const void *value1, const void *value2);
+void getValueMatrix();
+int randNumber(int low,int high);
+
+int matrix[10][10];
+
+int main() {
+    srand(time(NULL));
+    int i, j;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            matrix[i][j] = randNumber(1, 10);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+    cout << "\n";
+    cout << "Starting Array :\n";
+    cout << "\n";
+    getValueMatrix();
+    qsort(matrix, 100, sizeof(int), compareArray);
+
+    cout << "\n";
+    cout << "Quick Sorting Result :\n";
+    cout << "\n";
+    getValueMatrix();
+
+    return 0;
 }
- 
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        /* pi is partitioning index, arr[p] is now
-           at right place */
-        int pi = partition(arr, low, high);
- 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+
+int randNumber(int low,int high) {
+    if (low == high) {
+        return low;
+    }
+    else {
+        return (rand()%(high - low + 1)+ low);
     }
 }
- 
-/* Function to print an array */
-void printArray(int arr[], int size) {
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+
+int compareArray(const void *value1, const void *value2) {
+    int resultValue1 = *((int*) value1);
+    int resultValue2 = *((int*) value2);
+    return resultValue1 - resultValue2;
 }
- 
-// Driver program to test above functions
-int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5, 7, 9, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
+
+void getValueMatrix() {
+    int i, j;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            cout << setw(3) << matrix[i][j];
+        }
+        cout << endl;
+    }
 }
